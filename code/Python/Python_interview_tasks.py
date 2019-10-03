1fa7c4ebd1ec7d46c88343d8cb670e4d37709add
Dmytro Manannykov <mdmytro@Dmytros-MacBook-Pro.local>
2019-10-02 21:40:59 -0700

    new tasks

--- code/Python/Python_interview_tasks.py
+++ code/Python/Python_interview_tasks.py
@@ -342,7 +342,7 @@ Count all odd numbers in the list usig recursion
 ------------------------------------SOLUTION------------------------------------
 def countOdd(l):
 	print l
-	if l == list(): 
+	if l == list(): #if list is empty
 		return 0
 	return l[0] % 2 + countOdd(l[1:])
 
@@ -482,4 +482,62 @@ def occur_num(arr):
     return dic
 
 print occur_num(arr)
+================================================================================
+
+======================================TASK======================================
+Write a program named fizzbuzz that prints all integers in 1 through 10, inclusive. 
+For odd numbers print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are both odd and a multiple of five, print “FizzBuzz”:
+Fizz
+2
+Fizz
+4
+FizzBuzz
+6
+Fizz
+8
+Fizz
+Buzz
+------------------------------------SOLUTION------------------------------------
+list = range(1,11)
+
+def fizzbuzz(list):
+    for n in list:
+        if (n%2 > 0) & (n%5 == 0): 
+            print 'FizzBuzz'
+        elif n%5 == 0: 
+            print 'Buzz'
+        elif n%2 > 0: 
+            print 'Fizz'
+        else:
+            print n
+
+fizzbuzz(list)
+================================================================================
+
+======================================TASK======================================
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 
+and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers:
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+------------------------------------SOLUTION------------------------------------
+def sum67(list):
+    flag = False
+    sm = 0
+    for n in list:
+        if n == 6:
+            flag = True
+        elif n == 7:
+            flag = False
+        elif flag:
+            continue
+        else:
+            sm += n
+    print 'total: ' + str(sm)
+
+
+sum67([1, 2, 2])
+sum67([1, 2, 2, 6, 99, 99, 7])
+sum67([1, 1, 6, 7, 2])
 ================================================================================
